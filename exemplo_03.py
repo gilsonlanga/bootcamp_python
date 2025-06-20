53dde6844e3aaafff7737f22941d3eb3af226aeb
Gilson Langa <giu.langa@gmail.com>
2025-06-20 12:22:24 -0300

    adicionar exemplo 03

---
+++ exemplo_03.py
@@ -0,0 +1,9 @@
+# n1 = int(input("Digite o primeiro número: "))
+# n2 = int(input("Digite o segundo número: "))
+
+# print(n1 + n2)
+
+nome = input("Digite o seu nome: ")
+tamanho = str(len(nome))
+
+print(nome + " = " + tamanho + " caracteres")
